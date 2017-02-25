567eb02459c3fd22ac51b675db838f97cc336291
Mendes <mendes@mendes-pc.localdomain>
2017-02-25 03:02:20 -0300

    Usando o find_packages

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+from setuptools import find_packages
 
 with open('README.md') as f:
     long_description = f.read()
 
 setup(
     name='pybrdst',
-    packages=['pybrdst'],  # this must be the same as the name above
+    packages=find_packages(exclude=['tests']),
     version='0.1',
     description='Brazilian daylight saving time',
     long_description=long_description,
@@ -16,7 +17,6 @@ setup(
     url='https://github.com/joaocarlosmendes/pybrdst',
     download_url='https://github.com/joaocarlosmendes/'
         'pybrdst/releases/tag/v0.1',
-    packages=find_packages(exclude=['test*']),
     license='MIT',
     keywords=['DST',
               'brazilian',
